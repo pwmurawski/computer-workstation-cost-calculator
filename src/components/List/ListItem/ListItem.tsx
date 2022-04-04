dba9994ccf9929ca648184a7436d191fc33dbf5f
unknown <pwmurawski123@gmail.com>
2022-04-04 17:28:46 +0200

    list item data display has been added

--- src/components/List/ListItem/ListItem.tsx
+++ src/components/List/ListItem/ListItem.tsx
@@ -1,17 +1,80 @@
-import { ListItemContainer } from "./styles/ListItemStyles";
+/* eslint-disable no-unused-vars */
+import { useState } from "react";
+import getIconCategory from "../../../helpers/getIconCategory";
+import {
+  ListItemContainer,
+  Icon,
+  Desc,
+  Name,
+  Price,
+  Left,
+  Right,
+  Container,
+  DeleteBtn,
+} from "./styles/ListItemStyles";
+import ListItemEditForm from "./ListItemEditForm/ListItemEditForm";
+import deleteImg from "../../../assets/delete.png";
+import { IListItemData } from "../../../reducer";
 
 interface IListItemProps {
+  id: string;
   name: string;
   desc: string;
   price: string;
-  category: string;
+  category: {
+    name: string;
+    value: string;
+  };
+  deleteItemHandler: (id: string) => void;
+  editItemHandler: (id: IListItemData) => void;
 }
 
 export default function ListItem({
+  id,
   name,
   desc,
   price,
   category,
+  deleteItemHandler,
+  editItemHandler,
 }: IListItemProps) {
-  return <ListItemContainer>list item</ListItemContainer>;
+  const [showListItemEditForm, setshowListItemEditForm] = useState(false);
+
+  const submitHandler = (formData: IListItemData) => {
+    setshowListItemEditForm(false);
+    editItemHandler(formData);
+  };
+
+  if (showListItemEditForm)
+    return (
+      <ListItemEditForm
+        submitHandler={submitHandler}
+        defaultValues={{ id, name, desc, price, category }}
+      />
+    );
+  return (
+    <ListItemContainer
+      onDoubleClick={() => {
+        setshowListItemEditForm(true);
+      }}
+    >
+      <Left>
+        <Icon
+          width="50px"
+          height="50px"
+          src={getIconCategory(category.value)}
+        />
+        <Container>
+          <Name>{name}</Name>
+          <Desc>{desc}</Desc>
+        </Container>
+      </Left>
+      <Right>
+        <Price>{price} zł</Price>
+        <DeleteBtn onClick={() => deleteItemHandler(id)}>
+          <Icon width="100%" height="100%" src={deleteImg} />
+        </DeleteBtn>
+      </Right>
+    </ListItemContainer>
+  );
 }
--- src/components/List/ListItem/styles/ListItemStyles.tsx
+++ src/components/List/ListItem/styles/ListItemStyles.tsx
@@ -3,9 +3,13 @@ import styled from "styled-components";
 
 export const ListItemContainer = styled.li`
   display: flex;
+  justify-content: space-between;
   align-items: center;
-  height: 60px;
+  min-width: 230px;
+  height: fit-content;
+  min-height: 50px;
   padding: 12px 16px 12px 12px;
+  cursor: pointer;
 
   border-left: 1px solid #b8b8b8;
   border-right: 1px solid #b8b8b8;
@@ -18,5 +22,61 @@ export const ListItemContainer = styled.li`
   :last-of-type {
     border-bottom-left-radius: 10px;
     border-bottom-right-radius: 10px;
+  }
+`;
+
+export const Icon = styled.img``;
+
+export const Name = styled.div`
+  display: flex;
+  align-items: center;
+  font-weight: 600;
+  text-transform: capitalize;
+`;
+
+export const Desc = styled.div`
+  display: flex;
+  align-items: center;
+  font-size: 16px;
+
+  @media (max-width: 720px) {
+    font-size: calc(8px + 1vw);
+  }
+`;
+
+export const Price = styled.div`
+  display: flex;
+  align-items: center;
+`;
+
+export const Left = styled.div`
+  display: flex;
+  align-items: center;
+`;
+
+export const Right = styled.div`
+  display: flex;
+  align-items: center;
+`;
+
+export const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  margin: 0 10px;
+  word-wrap: break-word;
+`;
+
+export const DeleteBtn = styled.button`
+  background: transparent;
+  border: 0;
+  border-radius: 20px;
+  padding: 10px;
+  width: 40px;
+  height: 40px;
+  margin-left: 10px;
+  cursor: pointer;
+  :hover {
+    background-color: whitesmoke;
   }
 `;
