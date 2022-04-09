ac7f3c4396c5eee7a96a4cc32a4a891bb7d225ec
unknown <pwmurawski123@gmail.com>
2022-04-09 03:21:02 +0200

    refactoring styles

--- src/components/InputCustom/styles/InputCustomStyles.tsx
+++ src/components/InputCustom/styles/InputCustomStyles.tsx
@@ -9,10 +9,14 @@ export const InputCustomContainer = styled.div`
   height: 100%;
   margin: 0 5px;
   background: transparent;
+
+  @media (max-width: 700px) {
+    margin-bottom: 2px;
+  }
 `;
 
 export const Label = styled.label`
-  --height: 20px;
+  --height: 18px;
   position: absolute;
   height: var(--height);
   top: calc(50% - var(--height) / 2);
@@ -20,8 +24,10 @@ export const Label = styled.label`
   font-size: 13px;
   text-transform: capitalize;
   padding: 0 2px;
+  border-radius: 20px;
   color: gray;
   cursor: text;
+  transition: top 100ms;
 `;
 
 export const Input = styled.input`
@@ -30,14 +36,11 @@ export const Input = styled.input`
   height: 100%;
   border-radius: 10px;
   border: 1px solid #b8b8b8;
+  outline: none;
 
   :focus ~ ${Label}, :not(:placeholder-shown) ~ ${Label} {
     top: -9px;
     background-color: white;
     font-size: 12px;
-  }
-
-  @media (max-width: 700px) {
-    margin-bottom: 2px;
   }
 `;
